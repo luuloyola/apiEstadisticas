7fdc7dc799c5f3a2c2e096fead0d5e380312dce6
luuloyola <lucianaloyola30@gmail.com>
2025-10-07 20:49:46 -0300

    cambio de version de python compatible con render y psycopg2

--- main.py
+++ main.py
@@ -16,37 +16,6 @@ from db.connections import get_chat_session, transaction_stats
 from functions.principal import define_statistics
 
 app = FastAPI()
-chat1 = [[1,'hola', 'me siento mal', 'quiero apostar todo al rojo', 'mi papa me va a retar'],
-         [2,'hola', 'me siento bien pero mi amigo no', 'aposto todo a river y perdio', 'como lo ayudo'],
-         [3,'quiero ver taylor swift', 'no apuesto me estan obligando a hacer esto'],
-         [4,'hola como estas', 'sos tonto', 'me caes mal', 'voy a apostar igual no me importa'],
-         [5,'hola', 'me siento pesimo', 'quiero dejar de apostar pero no puedo']]
-
-def mock_messages():
-    list_session = dict()
-    for i in chat1:
-        list_session[i[0]] = []
-        for j in i:
-            if type(j) is str:
-                new_message = Message([j, i[0],j, datetime.now()])
-                list_session[i[0]].append(new_message)
-
-    return list_session
-
-def mock_get():
-    print("entre a mock")
-    chats = mock_messages()
-    stats = dict.fromkeys(chats.keys())
-    for key in chats:
-        print("estoy en el for de mock con chat numero ",key)
-        stats[key] = define_statistics(chats[key])
-    for i in stats:
-        for j in chats[i]:
-            print(j)
-        print(stats[i])
-
-mock_get()
-
 @app.post("/stats")
 async def post_stats():
     list_session = get_chat_session()
